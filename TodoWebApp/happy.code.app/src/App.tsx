854ea87ee9eb156d73420d38712b6657c741ad74
Deonte Johnson <Jet2kus84@gmail.com>
2024-10-22 22:46:52 -0500

    Add happy code app solution

---
+++ TodoWebApp/happy.code.app/src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+//import './App.css'
+import HappyCodeMsg from './components/HappyCodeMsg'
+
+function App() {
+  return (
+    <>
+     <HappyCodeMsg />
+    </>
+  )
+}
+
+export default App
