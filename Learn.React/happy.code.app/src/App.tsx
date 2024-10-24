1f9f4d6d824e2c527dc3a1f2a267df2760a71ec4
Deonte Johnson <Jet2kus84@gmail.com>
2024-10-23 23:35:04 -0500

    Rename project to Learn.React

---
+++ Learn.React/happy.code.app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+//import './App.css'
+import Gallery from './components/Gallery'
+import Profile from './components/Profile'
+
+function App() {
+  return (
+    <>
+          <Gallery />
+          <Profile />
+    </>
+  )
+}
+
+export default App
